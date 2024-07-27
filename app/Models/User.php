855c467142891d1d7eca8a45976c62021c973637
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-07-27 17:10:39 +0600

    fix: user CentralConnection issue

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Stancl\Tenancy\Database\Concerns\CentralConnection;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
+    use CentralConnection;
 
     /**
      * The attributes that are mass assignable.
