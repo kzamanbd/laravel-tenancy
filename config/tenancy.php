e278069dfbbd30078c6e34b2d7b6214b284620ea
Kamruzzaman <kzamanbn@gmail.com>
2025-07-15 00:13:21 +0600

    Update tenancy.php

--- config/tenancy.php
+++ config/tenancy.php
@@ -18,8 +18,6 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        'localhost',
-        '127.0.0.1',
         config('app.central_domain'),
     ],
 
