4bb2ca4790886940255fe0308e8be18678946e4b
Kamruzzaman <kzamanbn@gmail.com>
2025-07-15 23:27:50 +0600

    Refactor CSS structure and update dependencies
    Split and reorganized CSS into modular files for core components and features. Updated and added several dependencies in package.json, including Tailwind CSS plugins and UI libraries, and upgraded related packages. Adjusted JavaScript and Blade templates to align with the new CSS structure.

--- resources/views/livewire/tenants.blade.php
+++ resources/views/livewire/tenants.blade.php
@@ -22,9 +22,7 @@
                     <tbody>
                         @foreach ($this->tenants as $tenant)
                             <tr class="bg-white border-b">
-                                <th scope="row"
-                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap
-                                        Apple MacBook Pro 17">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900">
                                     {{ $tenant->name }}
                                 </th>
                                 <td class="px-6 py-4">
