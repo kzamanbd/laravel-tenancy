56d0e9cf620c873aa7ef6199ab5778477b1f2232
Kamruzzaman <kzamanbn@gmail.com>
2025-07-16 00:24:42 +0600

    refactor: update authentication views to use app logo component and improve layout

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,6 +1,8 @@
-<div class="flex aspect-square size-8 items-center justify-center rounded-md bg-gray-800 text-gray-100">
-    <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
-</div>
-<div class="ms-1 grid flex-1 text-start text-sm">
-    <span class="mb-0.5 truncate leading-tight font-semibold app-name break-words">{{ config('app.name') }}</span>
+<div {{ $attributes->merge(['class' => 'flex items-center gap-1.5']) }}>
+    <div class="flex aspect-square size-8 items-center justify-center rounded-md bg-gray-800 text-gray-100">
+        <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
+    </div>
+    <div class="grid flex-1 text-start text-sm">
+        <span class="mb-0.5 truncate leading-tight font-semibold app-name break-words">{{ config('app.name') }}</span>
+    </div>
 </div>
